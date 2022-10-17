c226b8f73aebb976801bcbc653316fbfb393d958
Sonam <sthukral6331@bths.edu>
2022-10-17 01:06:09 -0400

    hw

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
-        double random = Math.random() * 75 + 25;
-        System.out.println(random);
+        LuckyNumbers numberGenerator = new LuckyNumbers();
+        String luckyNums = numberGenerator.getLuckyNumbers();
+        System.out.println(luckyNums);
     }
 }
